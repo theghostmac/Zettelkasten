6d9fc97188b7d8e733f0827f9d521c90cd338e0a
MacBobby Chibuzor <theghostmac@gmail.com>
2023-05-26 21:59:19 +0100

    handled errors

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 // serveFirstPage defines a handler function for serving the first page.
 func serveFirstPage(writer http.ResponseWriter, reader *http.Request) {
-	writer.Write([]byte("Hello from inside the simple server..."))
+	_, err := writer.Write([]byte("Hello from inside the simple server..."))
+	if err != nil {
+		// Handle the error
+		fmt.Println("Error occurred:", err)
+	}
 }
 
 func main() {
-	// create a multiplexor.
+	// create a multiplexer.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", serveFirstPage)
 
